0cb47221c21e2eefd33244a20b8be3c7cf9fe7c6
MichaelNg97 <michaelngteckhua@gmail.com>
2016-09-10 20:47:22 -0400

    Add files via upload

---
+++ HelloWorld1.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld1;
+
+import java.util.Scanner;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Michael
+ */
+public class HelloWorld1 extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.printf("Hello World");
+        
+        System.out.printf("%nWhat is your name?");
+                
+        Scanner keyboard = new Scanner(System.in);
+        String userName = keyboard.nextLine();
+        
+        System.out.printf("%nHello" + userName);
+        
+        System.out.printf("Whatssup");
+        launch(args);
+    }
+    
+}
